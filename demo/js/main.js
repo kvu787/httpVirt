783231f350747246d459bd68bd7a2c84f28d57fe
Kevin Vu <kevin.m.vu@gmail.com>
2016-11-24 22:56:23 -0800

    Cleanup main.js

--- demo/js/main.js
+++ demo/js/main.js
@@ -3,15 +3,15 @@ function main() {
   var rows = 24;
   var term = new Terminal({ cursorBlink: true });
   var terminalContainer = document.getElementById('terminal-container');
-  // clean terminal
-  while (terminalContainer.children.length > 0) {
-    terminalContainer.removeChild(terminalContainer.children[0]);
-  }
+
   // associate JS term object with HTML div
   term.open(terminalContainer);
   var charWidth = Math.ceil(term.element.offsetWidth / cols);
   var charHeight = Math.ceil(term.element.offsetHeight / rows);
+
+  // create a backend container
   $.get('http://127.0.0.1:10411/create', function(containerID, status) {
+    // create a websocket to that contianer
     var socketURL = `ws://127.0.0.1:10411/xterm/${containerID}`;
     var webSocket = new WebSocket(socketURL);
     webSocket.onopen = function() {
@@ -25,11 +25,6 @@ function main() {
       console.log('Socket closed');
     };
   });
-
-  // GET localhost/create
-  // => containerID
-  // ws = WS localhost/session/${containerID}
-  // term.attach(ws)
 }
 
 main();
